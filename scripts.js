59097d341b95a4e5b59ff1068f764f20163f8e7e
Mateusz Widerski <mat.widerski@gmail.com>
2018-02-27 22:27:33 +0100

    Add index.html, style.css, scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var a = 2.45,
+	h = 12,
+	triangleArea;
+
+triangleArea = a*h/2;
+
+console.log('Triangle field with base a = ' + a + ' and height h = ' + h + ' is equal to: ' + triangleArea);
+
+a = prompt('Podaj wartość podstawy trójkąta.');
+h = prompt('Podaj wartość wysokości trójkąta.');
+
+triangleArea = a*h/2;
+
+console.log('Triangle field with base a = ' + a + ' and height h = ' + h + ' is equal to: ' + triangleArea);
